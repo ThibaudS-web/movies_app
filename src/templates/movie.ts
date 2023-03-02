459ca0e1f4e50eeb5fc5556b9d54002359d91aa2
ThibaudS <thibaud.devpro@gmail.com>
2023-03-02 11:17:27 +0100

    Applied destructuring on movie templates

--- src/templates/movie.ts
+++ src/templates/movie.ts
@@ -1,44 +1,42 @@
 import MovieLocal from "../models/MovieLocal"
 
 export const getMovieHTML = (movie: MovieLocal, node: HTMLElement) => {
+	const { Poster, Title, Writer, Country, Plot, Actors, imdbRating, Year, Runtime, Genre } = movie
+
 	node.innerHTML = `
             <div id="movie-container">
             <div id="movie-topside">
                 <div id="movie-img">
                     <img
-                        src=${
-							movie.Poster === "N/A" ? "../assets/image_not_found.png" : movie.Poster
-						}
+                        src=${Poster === "N/A" ? "../assets/image_not_found.png" : Poster}
                     />
                 </div>
                 <div id="movie-infos-topside">
-                    <h2>${movie.Title}</h2>
-                    <p id="year-duration">${movie.Year} - ${movie.Runtime}</p>
+                    <h2>${Title}</h2>
+                    <p id="year-duration">${Year} - ${Runtime}</p>
                     <div id="rating">
-                        ${movie.imdbRating}
+                        ${imdbRating}
                         <img id="star" src="../assets/star.png" alt="rating" />
                     </div>
                     <div id="genres"> 
-                        ${movie.Genre.map((genre) => `<div class="tag">${genre}</div>`).join("")}
+                        ${Genre.map((genre) => `<div class="tag">${genre}</div>`).join("")}
                     </div>
-                    <p id="country"><span class="label-infos">Country:</span> ${movie.Country}</p>
+                    <p id="country"><span class="label-infos">Country:</span> ${Country}</p>
                 </div>
             </div>
             <div id="movie-botside">
                 <p class="bloc-text">
-                    <span class="label-infos">Writer:</span> ${movie.Writer}
+                    <span class="label-infos">Writer:</span> ${Writer}
                 </p>
                 <p class="bloc-text">
                     <span class="label-infos">Plot:</span>
-                    ${movie.Plot}
+                    ${Plot}
                 </p>
                 <p class="bloc-text">
                     <span class="label-infos">Actors:</span>
-                    ${movie.Actors}
+                    ${Actors}
                 </p>
             </div>
         </div>
     `
 }
-
-
